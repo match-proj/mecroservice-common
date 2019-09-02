d7a2f85192e8c4e2319dc98141cd1746cbd31148
zhangchao <jaguar_zc@sina.com>
2019-09-02 18:27:52 +0800

    1.commit expenses

---
+++ src/main/java/com/match/common/utils/HttpClientResult.java
@@ -0,0 +1,54 @@
+package com.match.common.utils;
+
+/**
+ * @Author zhangchao
+ * @Date 2019/9/2 14:10
+ * @Version v1.0
+ */
+
+import java.io.Serializable;
+
+/**
+ * Description: 封装httpClient响应结果
+ *
+ * @author JourWon
+ * @date Created on 2018年4月19日
+ */
+public class HttpClientResult implements Serializable {
+
+    /**
+     * 响应状态码
+     */
+    private int code;
+
+    /**
+     * 响应数据
+     */
+    private String content;
+
+
+    public HttpClientResult(int code) {
+        this.code = code;
+    }
+
+    public HttpClientResult(int code, String content) {
+        this.code = code;
+        this.content = content;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
